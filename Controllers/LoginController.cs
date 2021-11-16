f35d3b72192931b4cbbd46de9e6ac051bdcbfe18
musamasood <55657714+musamasood@users.noreply.github.com>
2021-11-17 01:44:37 +0500

    Part 1

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using aleApiTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.UI.WebControls;
+
+namespace aleApiTest.Controllers
+{
+    public class LoginController : ApiController
+    {
+        private aleoApiTestEntities db = new aleoApiTestEntities();
+
+        // POST api/Login/UserLogin
+        [ResponseType(typeof(Login))]
+        public IHttpActionResult PostUserLogin(string username, string password)
+        {
+            //ClaimsIdentity ci = new ClaimsIdentity();
+            var checkuser = db.users.Where(x => x.userUsername == username && x.userPassword == password).FirstOrDefault();
+            if (checkuser != null)
+            {
+                Console.WriteLine("CORRECT UserName and Password");
+            }
+            else
+            {
+                Console.WriteLine("INCORRECT UserName or Password");
+            }
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+    }
+}
+
---
+++ Controllers/usersController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+using System.Web.Http.Description;
+using aleApiTest.Models;
+
+namespace aleApiTest.Controllers
+{
+    
+    public class usersController : ApiController
+    {
+        private aleoApiTestEntities db = new aleoApiTestEntities();
+
+        // GET: api/users
+        public IQueryable<user> Getusers()
+        {
+            return db.users;
+        }
+
+        // GET: api/users/5
+        [ResponseType(typeof(user))]
+        public IHttpActionResult Getuser(int id)
+        {
+            user user = db.users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        // PUT: api/users/10
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putuser(int id, user user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != user.idUser)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(user).State = System.Data.Entity.EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!userExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/users
+        [ResponseType(typeof(user))]
+        public IHttpActionResult Postuser(user user)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                db.users.Add(user);
+                db.SaveChanges();
+            }
+            catch (DbEntityValidationException e)
+            {
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                        eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                            ve.PropertyName, ve.ErrorMessage);
+                    }
+                }
+                throw;
+            }
+            return CreatedAtRoute("DefaultApi", new { id = user.idUser }, user);
+        }
+
+        // DELETE: api/users/5
+        [ResponseType(typeof(user))]
+        public IHttpActionResult Deleteuser(int id)
+        {
+            user user = db.users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            db.users.Remove(user);
+            db.SaveChanges();
+
+            return Ok(user);
+        }
+       
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool userExists(int id)
+        {
+            return db.users.Count(e => e.idUser == id) > 0;
+        }
+    }
+}
